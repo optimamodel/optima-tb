582cf17e52d04b7ccc42ff8a1a69bc4f8475c9bf
cliffckerr <questionably@gmail.com>
2016-09-14 10:02:18 +1000

    ok, this is what i meant to do

--- misc/multipletransitions.py
+++ misc/multipletransitions.py
@@ -2,28 +2,28 @@
 Demonstrates rationale for combining potentially incompatible transition rates.
 """
 
-from pylab import zeros, rand, log, exp
+from pylab import zeros, rand, log
 npeople = 100000 # Number of agents
-infrate = 0.5 # Infection rate
-vacrate = 0.5 # Vaccination probability
-timestep = exp(1)/2 # The timestep being considered
-bothallowed = True # Whether both infection and vaccination can happen
+infrate = 0.75 # Infection rate
+vacrate = 0.25 # Vaccination probability
+bothallowed = False # Whether both infection and vaccination can happen
 
 z = [0,0]
 people = zeros((npeople,2)) # List of agends, tracking infection and vaccination status
-inftimes = [] # Store times to infection for e.g. mean(array(inftimes<timestep))
-vactimes = []
 for p in range(npeople): # Loop over people
-    inftime = -1./infrate*log(1-rand())
-    vactime = -1./vacrate*log(1-rand())
-    inftimes.append(inftime)
-    vactimes.append(vactime)
-    doinf = True if inftime<timestep else False # See if infection happens
-    dovac = True if vactime<timestep else False # See if vaccination happens
-    if not bothallowed: # Whether or not to disallow both transitions
-        if doinf and dovac: # Check that both transitions are on the cards
-            if inftime<vactime: dovac = False # Infection takes priority
-            if vactime<inftime: doinf = False # Vaccination takes priority
+    infmove = rand()
+    vacmove = rand()
+    doinf = True if infmove<infrate else False # See if infection happens
+    dovac = True if vacmove<vacrate else False # See if vaccination happens
+    if doinf and dovac and not bothallowed: # Whether or not to disallow both transitions
+        timetoinf = -1./infrate*log(1-rand())
+        timetovac = -1./vacrate*log(1-rand())
+        if timetoinf<timetovac: 
+            dovac = False
+            z[0] += 1
+        else: 
+            doinf = False
+            z[1] += 1
     if doinf: people[p,0] = 1 # Update state
     if dovac: people[p,1] = 1 # Update state
 
@@ -41,8 +41,10 @@ analytic[0,1] = (1-infrate)*(  vacrate)
 if bothallowed:
     analytic[1,1] = (infrate)*(vacrate)
 else:
-    analytic[1,0] += (infrate)*(vacrate) * (infrate/(infrate+vacrate))
-    analytic[0,1] += (infrate)*(vacrate) * (vacrate/(infrate+vacrate))
+    proportion = (infrate)*(vacrate)
+    denominator = (infrate+vacrate)
+    analytic[1,0] += proportion * infrate/denominator
+    analytic[0,1] += proportion * vacrate/denominator
 print(analytic)
 
 print(fractions/analytic)
