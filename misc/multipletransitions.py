d22b6772e3b20cc7648056af8169ac26206baccd
David Kedziora <davidkedz@hotmail.com>
2016-09-21 12:36:22 +0100

    The actual exclusive transitions split
    It turns out that exclusive transitions need to be split in a special
    manner by rates, not probabilities.

--- misc/multipletransitions.py
+++ misc/multipletransitions.py
@@ -3,22 +3,36 @@ Demonstrates rationale for combining potentially incompatible transition rates.
 """
 
 # Define things
-from pylab import zeros, rand, log
-npeople = 100000 # Number of agents
-infrate = 0.75 # Infection rate
-vacrate = 0.25 # Vaccination probability
+from pylab import zeros, rand, log, exp
+npeople = 1000000 # Number of agents
+infrate = 2.0 # Infection rate (average number of events per year, can be over 1)
+vacrate = 0.5 # Vaccination rate (average number of events per year, can be over 1)
 bothallowed = False # Whether both infection and vaccination can happen
+timestep = 0.5 # Timestep for which probability of transition is calculated 
+infprob = 1-exp(-infrate*timestep)
+vacprob = 1-exp(-vacrate*timestep)
 
 # Run simulation
 people = zeros((npeople,2)) # List of agends, tracking infection and vaccination status
 for p in range(npeople): # Loop over people
-    doinf = True if rand()<infrate else False # See if infection happens
-    dovac = True if rand()<vacrate else False # See if vaccination happens
-    if doinf and dovac and not bothallowed: # Whether or not to disallow both transitions
-        timetoinf = -1./infrate*log(1-rand()) # Calculate time to infection
-        timetovac = -1./vacrate*log(1-rand()) # Calculate time to vaccination
-        if timetoinf<timetovac: dovac = False # Infection came first
-        else:                   doinf = False # Vaccination came first
+    timetoinf = -1./infrate*log(1-rand()) # Calculate time to infection
+    timetovac = -1./vacrate*log(1-rand()) # Calculate time to vaccination
+
+    doinf = False
+    dovac = False
+    if timetoinf < timestep: doinf = True
+    if timetovac < timestep: dovac = True
+    if not bothallowed:
+        if timetoinf > timetovac: doinf = False
+        else: dovac = False
+    
+#    doinf = True if rand()<infrate else False # See if infection happens
+#    dovac = True if rand()<vacrate else False # See if vaccination happens
+#    if doinf and dovac and not bothallowed: # Whether or not to disallow both transitions
+#        timetoinf = -1./infrate*log(1-rand()) # Calculate time to infection
+#        timetovac = -1./vacrate*log(1-rand()) # Calculate time to vaccination
+#        if timetoinf<timetovac: dovac = False # Infection came first
+#        else:                   doinf = False # Vaccination came first
     if doinf: people[p,0] = 1 # Update state
     if dovac: people[p,1] = 1 # Update state
 
@@ -32,16 +46,18 @@ print('Simulation:\n%s\n' % simulation)
 
 # Calculate equivalent analytic result
 analytic = zeros((2,2)) 
-analytic[0,0] = (1-infrate)*(1-vacrate) # People who don't move
-analytic[1,0] = (  infrate)*(1-vacrate) # People who are infected
-analytic[0,1] = (1-infrate)*(  vacrate) # People who are vaccinated
+analytic[0,0] = (1-infprob)*(1-vacprob) # People who don't move
+analytic[1,0] = (  infprob)*(1-vacprob) # People who are infected
+analytic[0,1] = (1-infprob)*(  vacprob) # People who are vaccinated
 if bothallowed:
-    analytic[1,1] = (infrate)*(vacrate) # People who are both infected and vaccinated
+    analytic[1,1] = (infprob)*(vacprob) # People who are both infected and vaccinated
 else:
-    proportion = (infrate)*(vacrate) # People who would've been both infected and vaccinated
-    denominator = (infrate+vacrate) # Normalization for rates
-    analytic[1,0] += proportion * infrate/denominator # Additional people who are infected
-    analytic[0,1] += proportion * vacrate/denominator # Additional people who are vaccinated
+    proportion = infprob + vacprob - infprob*vacprob # People who are either infected or vaccinated... or both
+    infsplit = infrate
+    vacsplit = vacrate
+    denominator = (infsplit+vacsplit) # Normalization for rates
+    analytic[1,0] = proportion * infsplit/denominator # People who are infected
+    analytic[0,1] = proportion * vacsplit/denominator # People who are vaccinated
 print('Analytic:\n%s\n' % analytic)
 
 # Check how well they match up
