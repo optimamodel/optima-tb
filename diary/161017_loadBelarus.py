49897f02a26a635c27e8a450298de7cb00226f54
critcortex <criticalcortex@googlemail.com>
2016-10-21 18:54:35 +1100

    Updated so that now reads in pseudo-correct data from Belarus.

--- diary/161017_loadBelarus.py
+++ diary/161017_loadBelarus.py
@@ -6,18 +6,20 @@ pp = pprint.PrettyPrinter(indent=4)
 
 
 
+
+
+proj= Project(name = 'test-Belarus', cascade_path = '../data/cascade.xlsx')
 #proj.makeSpreadsheet(num_pops = 5)
 
-
-proj= Project(name = 'test-Belarus', cascade_path = '../data/cascade-simple.xlsx')
-proj.loadSpreadsheet(databook_path = '../diary/test-Belarus-data.xlsx')
+# take test-Belarus-data.xlsx, set values and save as Belarus-cascade-data.xlsx (so that no danger of overwriting)
+proj.loadSpreadsheet(databook_path = '../diary/Belarus-cascade-data.xlsx')
 
 proj.makeParset()
 print proj.data
-#r1, o1 = proj.runSim()
+r1, o1 = proj.runSim(plot=True)
 
 
-data = load_spreadsheet('../data/Belarus data entry sheet v4a.xlsx')
-print data.keys()
-pp.pprint(data)
-print proj.settings
+#data = load_spreadsheet('../data/Belarus data entry sheet v4a.xlsx')
+#print data.keys()
+#pp.pprint(data)
+#print proj.settings
