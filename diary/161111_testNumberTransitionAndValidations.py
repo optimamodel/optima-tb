3f27234446ce05a0373fda74623a2d969bb81398
critcortex <criticalcortex@googlemail.com>
2016-11-18 16:31:35 +1100

    Files for testing and running validation of negative populations and dpop changes using Number rather than Fraction values.

--- diary/161111_testNumberTransitionAndValidations.py
+++ diary/161111_testNumberTransitionAndValidations.py
@@ -8,49 +8,65 @@ import pylab
 Aim: test and validation that implementation of number format for input transitions 
 
 """
+
 num_pop = 2
-databook = './data/databook-simple-cascadeNumber.xlsx'
-  
- 
-proj= Project(name = 'test-Belarus-simple', cascade_path = 'data/cascade-simple.xlsx')
- 
-# 1. make spreadsheet after implementing number format
-#proj.makeSpreadsheet(databook_path=databook2, num_pops = num_pop)
+plot = False
+validateNegNumbers = False
 
 
-# 2, load spreadsheet with number format
-proj.loadSpreadsheet(databook_path = databook)
- 
- 
-proj.makeParset()
-#print proj.data
-r1, o1 = proj.runSim(plot=False)
+def runSimpleCascade(validation_lvl):
+    
+    databook = './data/databook-simple-cascadeNumber.xlsx'
+     
+    proj= Project(name = 'test-Belarus-simple', cascade_path = 'data/cascade-simple.xlsx', validation_level = validation_lvl)
+    proj.setYear([2000.,2031.],False) # simulate to 2031
+    
+    # 1. make spreadsheet after implementing number format
+    #proj.makeSpreadsheet(databook_path=databook, num_pops = num_pop)
+    
+    
+    # 2, load spreadsheet with number format
+    proj.loadSpreadsheet(databook_path = databook)
+     
+     
+    proj.makeParset()
+    #print proj.data
+    r1, o1 = proj.runSim(plot=plot)
+    
 
 
-""" 
-# Check with full cascade
-databook2 = './data/databook-full-cascade.xlsx'
- 
- 
-proj2= Project(name = 'test-Belarus-full', cascade_path = 'data/cascade.xlsx')# './diary/cascade-161025.xlsx')
-proj2.setYear([2000.,2016.])
 
-# 1. make spreadsheet after implementing number format
-#proj2.makeSpreadsheet(databook_path=databook2, num_pops = num_pop)
- 
-# 2, load spreadsheet with number format
-proj2.loadSpreadsheet(databook_path = databook2)
- 
-  
-proj2.makeParset()
-# print proj2.data
-ss = proj2.settings
-ps = proj2.parsets[0]
+def runFullCascade(validation_lvl):
+    # Check with full cascade
+    databook2 = './data/databook-full-cascade.xlsx'
+     
+     
+    proj2= Project(name = 'test-Belarus-full', cascade_path = 'data/cascade.xlsx', validation_level = validation_lvl) 
+    proj2.setYear([2000.,2016.])
+    
+    # 1. make spreadsheet after implementing number format
+    #proj2.makeSpreadsheet(databook_path=databook2, num_pops = num_pop)
+     
+    # 2, load spreadsheet with number format
+    proj2.loadSpreadsheet(databook_path = databook2)
+     
+      
+    proj2.makeParset()
+    # print proj2.data
+    ss = proj2.settings
+    ps = proj2.parsets[0]
+    
+    
+    r1, o1 = proj2.runSim(plot=plot) 
 
 
-r1, o1 = proj2.runSim(plot=False) 
-"""
+for vlvl in ['ignore','avert','warn','error']:
+    """ should only halt when vlvl = error """ 
+    #plot=True
+    runSimpleCascade(vlvl)
+
+    pylab.show()
 
 
-pylab.show()
+
 
