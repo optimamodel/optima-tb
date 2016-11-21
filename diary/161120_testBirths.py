9e29ebb1d5118e6940133cce21fba4dd5a846376
critcortex <criticalcortex@googlemail.com>
2016-11-21 18:28:49 +1100

    Sample cascade.xlsx (cascade-simple-births.xlsx), completed databook, and code to run cascade that includes births.

---
+++ diary/161120_testBirths.py
@@ -0,0 +1,70 @@
+
+from project import Project
+
+import pylab 
+
+"""
+Aim: test and validation that implementation of births
+
+"""
+
+num_pop = 2
+plot = False
+
+
+def runSimpleCascade():
+    
+    databook = './data/databook-simple-cascadeBirths.xlsx'
+     
+    proj= Project(name = 'test-Belarus-simple', cascade_path = 'data/cascade-simple-births.xlsx')
+    proj.setYear([2000.,2015.],False) # simulate to 2031
+    """
+    # 1. make spreadsheet after implementing number format
+    proj.makeSpreadsheet(databook_path=databook, num_pops = num_pop)
+    
+    """
+    # 2, load spreadsheet with number format. 
+    # Test by setting aging --> n
+    proj.loadSpreadsheet(databook_path = databook)
+     
+     
+    proj.makeParset()
+    #print proj.data
+    r1, o1 = proj.runSim(plot=plot)
+    
+    
+
+
+
+def runFullCascade(validation_lvl):
+    # Check with full cascade
+    databook2 = './data/databook-full-cascade.xlsx'
+     
+     
+    proj2= Project(name = 'test-Belarus-full', cascade_path = 'data/cascade.xlsx', validation_level = validation_lvl) 
+    proj2.setYear([2000.,2016.])
+    
+    # 1. make spreadsheet after implementing number format
+    #proj2.makeSpreadsheet(databook_path=databook2, num_pops = num_pop)
+     
+    # 2, load spreadsheet with number format
+    proj2.loadSpreadsheet(databook_path = databook2)
+     
+      
+    proj2.makeParset()
+    # print proj2.data
+    ss = proj2.settings
+    ps = proj2.parsets[0]
+    
+    
+    r1, o1 = proj2.runSim(plot=plot) 
+
+
+plot=True
+runSimpleCascade()
+
+pylab.show()
+
+
+
+
