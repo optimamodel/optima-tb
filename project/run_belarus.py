c5baeee3f072094fd5f67bb2d5a6c41f95c8bf5a
Dr Lara Gosce <l.gosce@ucl.ac.uk>
2016-11-29 13:35:57 +0000

    cascade
    py file

---
+++ project/run_belarus.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 29 13:12:24 2016
+
+@author: Lara
+"""
+
+#new file
+import sys
+sys.path.append('../optima')
+
+from project import Project
+
+proj= Project(name = 'Belarus', cascade_path = './cascade_belarus.xlsx')
