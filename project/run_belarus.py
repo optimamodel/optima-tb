99eb2128926037e81dcb135aed1f007c0008031e
David Kedziora <davidkedz@hotmail.com>
2016-12-06 20:44:00 +0000

    Renaming for consistency
    Just filename changes. Underscores generally avoided by DJK unless part
    of code or script names.

--- project/run_belarus.py
+++ project/run_belarus.py
@@ -12,17 +12,17 @@ sys.path.append('../optima')
 from project import Project
 import pylab
 
-proj = Project(name = 'Belarus', cascade_path = './cascade_belarus.xlsx')
-proj.makeSpreadsheet(databook_path = './databook_belarus_template.xlsx', num_pops = 6, num_migrations = 2)
+proj = Project(name = 'Belarus', cascade_path = './cascade-belarus.xlsx')
+proj.makeSpreadsheet(databook_path = './databook-belarus-template.xlsx', num_pops = 6, num_migrations = 2)
 
 
 
-proj.loadSpreadsheet(databook_path = './databook_belarus_template.xlsx')
-proj.makeParset()
+proj.loadSpreadsheet(databook_path = './databook-belarus-template.xlsx')
+proj.makeParset(name = 'default')
 
 # run and plot simulations
 r1, o1, s1, results1 = proj.runSim(parset_name = 'default', plot = True)
-proj.runAutofitCalibration(new_parset_name='bob')
-r2, o2, s2, results2 = proj.runSim(parset_name = 'autocalibrated', plot = True)
+#proj.runAutofitCalibration(new_parset_name='autocalibrated')
+#r2, o2, s2, results2 = proj.runSim(parset_name = 'autocalibrated', plot = True)
 
 pylab.show()
