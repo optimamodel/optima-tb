fb1f648ca588d293181798b85d9ccf9fbb01f2b7
Azfar <hussainazfar>
2017-01-12 11:35:11 +1100

    Skeleton for cost coverage component

--- optima/programs.py
+++ optima/programs.py
@@ -1,4 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 12 11:32:07 2017
+
+@author: Azfar
+"""
+
 from uuid import uuid4 as uuid
+from utils import odict
 
 class ProgramSet:
     
@@ -9,7 +17,6 @@ class ProgramSet:
         
 
 class Program:
-    
     def __init__(self,name,label,duration,category=None):
         """
         
@@ -21,8 +28,19 @@ class Program:
         self.duration = duration
         self.category = category
         
-        self.cost_coverage = []
-        
+        self.cost_cov = CostCovOut()
+    
+    def getCoverage():
+        '''Returns coverage for a time/spending array'''
+        pass
+    
+    def getBudget():
+        '''Returns budget for a time/coverage array'''
+        pass
+    
+    def plotCostCov():
+        ''' Plot the cost-coverage curve for a single program'''
+        pass
         
 class TreatmentProgram(Program):
     
@@ -39,4 +57,43 @@ class TestingProgram(Program):
         super(TreatmentProgram,self).init(*args)
         self.specificity = specificity
         self.sensitivity = sensitivity
+
+class CostCovOut(object):
+    '''
+    Cost-coverage, coverage-outcome and cost-outcome objects
+    '''
+    def __init__(self):
+        self.cost_data = {'t': [], 'spending': [], 'coverage': []}
+        self.ccopar = {'t': [], 'saturation': [], 'unitcost': []}
+    
+    def addcostdata(self, cost_data, overwrite=False):
+        '''add or replace spending-coverage data'''
+        pass
+    
+    def rmcostdata(self, t):
+        '''remove spending-coverage data against a given year'''
+        pass
+    
+    def getcostdata(self, t):
+        '''retrieve/return odict of cost-coverage data'''
+        pass
+    
+    def addccopar(self, ccopars, overwrite=False):
+        '''add or replace cost-coverage parameters'''
+        pass
+    
+    def rmccopar(self, t):
+        '''remove cost-coverage parameters'''
+        pass
+    
+    def getccopar(self, t):
+        '''retrieve/return odict of cost-coverage parameters'''
+        pass
+    
+    def costcov(budget, costCovInfo, popsize, eps=None):
+        '''Function that calculates and returns coverage in a given year for a given spending amount.'''
+        pass
         
+    def inversecostcov(current_coverage, costCovInfo, popsize, eps=None):
+        '''Function that calculates and returns budget in a given year for a given coverage amount.'''
+        pass
