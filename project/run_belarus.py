e97cfd99be18e85945633d1c4b6e0b89929b2a11
Gerard J Abou Jaoude <gerard.jaoude.15@ucl.ac.uk>
2017-01-16 23:26:10 +0000

    Attempts for Demographic Fit
    Calculations mentioned in the e-mail are in the databook-belarus sheet,
    with the perfect scenario of 0 difference left to see and compare
    against the original values below.
    
    Updated the SP Untreated Death rates in the cascade sheet according to
    Lara’s e-mail.
    
    Line added to project/run_belarus.py by David to change time-step to 1
    temporarily for these test runs, and 1995 start date changed to 2000.
    
    Hoping this is clear!

--- project/run_belarus.py
+++ project/run_belarus.py
@@ -9,9 +9,10 @@ from optima_tb.project import Project
 import pylab
 
 proj = Project(name = 'Belarus', cascade_path = './cascade-belarus.xlsx', validation_level = 'avert')
+proj.settings.tvec_dt = 1.0
 
 #set the year range we simulate over as starting in 1995:
-proj.setYear([1995,2030],False)
+proj.setYear([2000,2030],False)
 
 
 proj.loadSpreadsheet(databook_path = './databook-belarus.xlsx')
