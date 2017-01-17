8679de2070c177ef0c277f9235466fe61062efff
Azfar <hussainazfar>
2017-01-17 15:05:37 +1100

    validate flows from births to deaths

--- diary/170117_validateCascade.py
+++ diary/170117_validateCascade.py
@@ -2,8 +2,8 @@
 Cascade validation check
 
 Required: 
-    The following file are required for this test
-        1. 'tb-ucl\diary\validate_cascade.xlsx'
+    The following file is required for this test:
+        1. 'tb-ucl\data\validate_cascade.xlsx'
           
 @date:   17-Jan-2017
 @author: hussainazfar
@@ -12,9 +12,9 @@ Required:
 from optima_tb.project import Project
 import os
 
-cascade =  os.path.abspath('../diary/validate_cascade.xlsx')
+cascade =  os.path.abspath('../data/validate_cascade.xlsx')
 
 #cascade validation is automatically run  when a project is created
 proj= Project(name = 'validate-cascade', cascade_path = cascade)
 #verify cascade through plot
-#proj.settings.plotCascade()
+proj.settings.plotCascade()
