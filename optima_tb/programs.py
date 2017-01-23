8359de5f945857a20c4381f7e559451a876c3e55
critcortex <criticalcortex@googlemail.com>
2017-01-23 18:27:01 +1100

    Minor change to skeleton

--- optima_tb/programs.py
+++ optima_tb/programs.py
@@ -17,7 +17,7 @@ class ProgramSet:
         
 
 class Program:
-    def __init__(self,name,label,duration,category=None):
+    def __init__(self,name,label,category=None):
         """
         
         """
@@ -29,16 +29,23 @@ class Program:
     
         self.cost_cov = CostCovOut()
     
-    def getCoverage():
-        '''Returns coverage for a time/spending array'''
+    
+    
+    def getCoverage(self,budget):
+        '''Returns coverage for a time/budget array'''
         pass
     
-    def getBudget():
+    def getBudget(self,coverage):
         '''Returns budget for a time/coverage array'''
         pass
     
-    def plotCostCov():
+    def getImpacts(self,budget):
+        ''' Returns impacts for a time/budget array'''
+        pass
+    
+    def plotCostCov(self):
         ''' Plot the cost-coverage curve for a single program'''
+        # should pass data to plotting.py
         pass
         
 class TreatmentProgram(Program):
@@ -93,11 +100,11 @@ class CostCovOut:
         '''retrieve/return odict of cost-coverage parameters'''
         pass
     
-    def calcCostCoverage(budget, costCovInfo, popsize, eps=None):
+    def calcCostCoverage(self, budget, costCovInfo, popsize, eps=None):
         '''Function that calculates and returns coverage in a given year for a given spending amount.'''
         pass
         
-    def calcInverseCostCoverage(current_coverage, costCovInfo, popsize, eps=None):
+    def calcInverseCostCoverage(self, current_coverage, costCovInfo, popsize, eps=None):
         '''Function that calculates and returns budget in a given year for a given coverage amount.'''
         pass
     
@@ -105,19 +112,23 @@ class CostCovOut:
     
     
 class CostCoverageCurve:
+    
+    import math
     """
     
     Curves currently supported: 
     1) exponentially saturating, from (0,0)
     2) exponentially saturating with offset, i.e. curve only defined for >= (offset, 0); 0, otherwise
     3) sigmoidal
+    4) fixed cost
     
     """
     
-    def __init__(self,curve_shape="exponential",**params):
+    def __init__(self,curve_shape="exponential",params={'exponent':1.,'saturation':1e6}):
         
         try:
             self.curve_shape = curve_shape
+            self.curve_params = params
             self.curve = getattr(self, '_curve_%s'%curve_shape)(params)
         except:
             raise NotImplementedError("No curve associated with _curve_%s (programs.py)"%curve_shape)
@@ -133,8 +144,8 @@ class CostCoverageCurve:
             raise NotImplementedError("No curve associated with _curve_%s (programs.py)"%curve_shape)
        
     """ Internal representations of curves """
-    def _curve_exponential(self, exponent, saturation):
-        pass
+    def _curve_exponential( exponent, saturation):
+        return saturation*(1-math.exp(-exponent))
     
     def _inverse_curve_exponential(self,exponent,saturation):
         pass
@@ -150,3 +161,12 @@ class CostCoverageCurve:
 
     def _inverse_curve_sigmoidal(self,sigma,saturation):
         pass
+    
+    def _curve_fixed(self,fixed):
+        pass
+    
+    def _inverse_curve_fixed(self,fixed):
+        pass
+    
+ 
+    
