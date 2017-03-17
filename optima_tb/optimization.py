6148057e8ef81f2880899699dca840de726f914b
David Kedziora <davidkedz@hotmail.com>
2017-03-17 20:09:38 +1100

    Default outcome calculation
    Default outcome to minimise is the negative of
    settings.charac_pop_count, as 'alive' or an alias will always exist for
    each cascade.

--- optima_tb/optimization.py
+++ optima_tb/optimization.py
@@ -6,7 +6,9 @@ import logging.config
 logging.config.fileConfig('logging.ini', disable_existing_loggers=False)
 logger = logging.getLogger()
 
+import numpy as np
 
+#def calculateOutcome()
 
 def optimizeFunc(settings, parset, progset, options = None):
     
@@ -26,14 +28,22 @@ def optimizeFunc(settings, parset, progset, options = None):
         if prog.label not in options['init_alloc']:
             options['init_alloc'][prog.label] = prog.getDefaultBudget()
         total_budget += options['init_alloc'][prog.label]
+        
+    if 'outcome_weight' not in options:
+        options['outcome_weight'] = {settings.charac_pop_count:'-1'}
             
     print options
     print total_budget
 
     results = runModel(settings = settings, parset = parset, progset = progset, options = options)
     
-    return results 
-        
-#options['init_alloc'] = {'HT-DS':1e6,'SAT-DS':1e6,'HT-MDR':1e6}     # Which programs will have user-specified initial budgets?
-      
-        
+    index_start = np.where(results.sim_settings['tvec'] >= options['progs_start'])[0][0]
+    print index_start
+    print results.sim_settings['tvec'][index_start:]
+    charac_label = options['outcome_weight'].keys()[0]
+    outcome = 0
+    for pop_label in results.outputs[charac_label].keys():
+    print outcome
+    
+    return results
+    
