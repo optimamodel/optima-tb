de147b9b2fce2e0f31db9aa00652631642cad01f
David Kedziora <davidkedz@hotmail.com>
2017-03-19 13:43:56 +1100

    Default constraints implemented
    It currently takes about 2 minutes to run 7 optimisation iterations for
    the Belarus project.
    If the optimizer tests the same allocation, the expensive model run
    should be averted in objective calculation, but this is yet to be
    verified.
    Either way, massive performance investigations are required, but we may
    need to forego that for the upcoming deadline and rely on
    multiprocessing boosts.
    In any case, this appears to be the absolute minimalist version of
    optimisation. Lots more work required to make it flexibly usable.

--- optima_tb/optimization.py
+++ optima_tb/optimization.py
@@ -1,6 +1,6 @@
 from optima_tb.asd import asd
 from optima_tb.model import runModel
-from optima_tb.utils import odict
+from optima_tb.utils import odict, OptimaException
 
 import logging
 import logging.config
@@ -10,22 +10,74 @@ logger = logging.getLogger()
 import numpy as np
 from copy import deepcopy as dcp
 
+def constrainAllocation(alloc, options):
 
-def calculateObjective(alloc, settings, parset, progset, options):
+    alloc = np.array(alloc)     # Converting to np array just in case.
+
+    # Convert negative allocation values to zeros.
+    alloc[alloc < 0.0] = 0.0    
     
-    options_iter = dcp(options)
-    for k in xrange(len(alloc)):
-        options_iter['init_alloc'][k] = alloc[k]
+    if 'total' in options['constraints']:
+        sum_current = sum(alloc)
+        if sum_current == 0: raise OptimaException('ERROR: Allocation was constrained to have a sum of zero during optimization.')
+        alloc *= options['constraints']['total']/sum_current
+        
     
-    results = runModel(settings = settings, parset = parset, progset = progset, options = options_iter)
+    return dcp(alloc)
+
+def calculateObjective(alloc, settings, parset, progset, options, algorithm_refs = None):
+    '''
+    Calculates the objective function value for a certain allocation.
+    The algorithm_refs dictionary is for storing algorithm-useful values during the process, e.g. the previous allocation tested.
+    '''
+    
+    print 'Unconstrained alloc...'
+    print alloc
+    print sum(alloc)
+    
+    alloc = constrainAllocation(alloc = alloc, options = options)   
+    
+    print 'Constrained alloc...'
+    print alloc
+    print sum(alloc)
+    
+    # Makes sure that time is not wasted on an allocation that was run in the previous step.
+    # Avoids useless iteration tests where a program is given negative funding, then constrained to be zero once more.
+    # This paragraph is for checking identical allocations.    
+    identical_alloc = False
+    if 'previous_alloc' in algorithm_refs:
+        if not algorithm_refs['previous_alloc'] is None:
+            identical_alloc = True
+            for k in xrange(len(alloc)):
+                if not alloc[k] == algorithm_refs['previous_alloc'][k]:
+                    identical_alloc = False
+                    break
+        algorithm_refs['previous_alloc'] = dcp(alloc)   # Store the new allocation for the next iteration.
+    
+    # This if-else clause prevents the actual expensive model run.
+    if identical_alloc and 'previous_results' in algorithm_refs and not algorithm_refs['previous_results'] is None:
+        results = algorithm_refs['previous_results']
+        logger.warn("Optimization iteration is testing the same allocation. Skipping model run.")
+    else:
+        options_iter = dcp(options)
+        for k in xrange(len(alloc)):
+            options_iter['init_alloc'][k] = alloc[k]
+        results = runModel(settings = settings, parset = parset, progset = progset, options = options_iter)
+
+    if 'previous_results' in algorithm_refs:
+        algorithm_refs['previous_results'] = dcp(results)   # Store the new results for the next iteration.
     
     index_start = np.where(results.sim_settings['tvec'] >= options['progs_start'])[0][0]
 #    print index_start
 #    print results.sim_settings['tvec'][index_start:]
-    charac_label = options['objective_weight'].keys()[0]
-    objective = 0
-    for pop_label in results.outputs[charac_label].keys():
-        objective += sum(results.outputs[charac_label][pop_label][index_start:])*results.dt*options['objective_weight'][charac_label]
+    objective = 0.0
+    
+    for objective_label in options['objectives']:
+#        charac_label = options['objective_weight'].keys()[0]
+        weight = 1.0
+        if 'weight' in options['objectives'][objective_label]: weight = options['objectives'][objective_label]['weight']
+        for pop_label in results.outputs[objective_label].keys():
+            objective += sum(results.outputs[objective_label][pop_label][index_start:]) * results.dt * weight
 #    print objective
     
     return objective
@@ -44,17 +96,21 @@ def optimizeFunc(settings, parset, progset, options = None):
     if 'init_alloc' not in options:
         options['init_alloc'] = {}
         
-    total_budget = 0
     for prog in progset.progs:
         if prog.label not in options['init_alloc']:
             options['init_alloc'][prog.label] = prog.getDefaultBudget()
-        total_budget += options['init_alloc'][prog.label]
     
-    # Convert alloc into an ordered dictionary.
+    # Convert alloc into an ordered dictionary so that keys and values are ordered during optimisation.
     options['init_alloc'] = odict(options['init_alloc'])
+    
+    # If user has not supplied constraints, then make the optimisation a redistribution of default budgets.
+    if 'constraints' not in options:
+        options['constraints'] = {}
+    if 'total' not in options:
+        options['constraints']['total'] = sum(options['init_alloc'].values())
         
-    if 'objective_weight' not in options:
-        options['objective_weight'] = {settings.charac_pop_count : -1.0}
+    if 'objectives' not in options:
+        options['objectives'] = {settings.charac_pop_count : {'weight':-1}}
             
 #    print options
 #    print total_budget
@@ -73,10 +129,11 @@ def optimizeFunc(settings, parset, progset, options = None):
     args = {'settings':settings,
             'parset':parset, 
             'progset':progset,
-            'options':options}
+            'options':options,
+            'algorithm_refs':{'previous_alloc':None, 'previous_results':None}}
     
-    alloc = dcp(options['init_alloc'].values())
-    alloc_new, obj_vals, exit_reason = asd(calculateObjective, alloc, args=args, maxiters=3)#, xmin=xmin, maxtime=maxtime, maxiters=maxiters, verbose=verbose, randseed=randseed, label=thislabel, **kwargs)
+    alloc = dcp(np.array(options['init_alloc'].values()))
+    alloc_new, obj_vals, exit_reason = asd(calculateObjective, alloc, args=args, maxiters=7)#, xmin=xmin, maxtime=maxtime, maxiters=maxiters, verbose=verbose, randseed=randseed, label=thislabel, **kwargs)
     
 #    print alloc_new
 #    print obj_vals
