120be65830ad427cd500ef94077d930e96e71ffc
critcortex <criticalcortex@googlemail.com>
2017-03-30 19:45:23 +1100

    Stack plot edges = same color

--- optima_tb/plotting.py
+++ optima_tb/plotting.py
@@ -1389,7 +1389,7 @@ def _plotStackedCompartments(tvec,comps,labels=None,datapoints=None,title='',yla
         lw = 0
         if save_fig:
             lw = 0.1
-        ax.fill_between(tvec, bottom, top, facecolor=colors[k], alpha=1,lw=lw) # for some reason, lw=0 leads to no plot if we then use fig.savefig()
+        ax.fill_between(tvec, bottom, top, facecolor=colors[k], alpha=1,lw=lw, edgecolors=colors[k]) # for some reason, lw=0 leads to no plot if we then use fig.savefig()
         reg, = ax.plot((0, 0), (0, 0), color=colors[k], linewidth=10)
         bottom = dcp(top)
         
