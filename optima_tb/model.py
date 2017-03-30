3db64a51e488bdcce41e1d71fb34f9ca68140185
David Kedziora <davidkedz@hotmail.com>
2017-03-30 14:08:39 +1100

    Fix to the ramp constraint check
    Ramp logic only should be processed if the average derivative (not
    multiple) across a number of years since starting programs is greater
    than the yearly constraint. (Remember that there is no reason not to
    allow the maximal change towards the intended allocation.)
    This commit fixes that check so that the zeroth timestep after program
    activation does not ignore the constraint.

--- optima_tb/model.py
+++ optima_tb/model.py
@@ -738,7 +738,7 @@ class Model(object):
                                         direction = np.sign(prog_budget - default_budget)
                                         
                                         # Only tests ramp restrictions if the change allowable each timestep is sufficiently small.
-                                        if eps <= np.abs(prog_budget - default_budget)*d_years:
+                                        if eps <= np.abs(prog_budget - default_budget)/(d_years + project_settings.TOLERANCE):  # Tolerance to avoid the divide by zero case.
                                             if relative_yearly_change is True:
                                                 if abs(default_budget) < project_settings.TOLERANCE:
     #                                                logger.warn('Default budget for "%s" is effectively zero and max yearly change is flagged as relative. Change in program funding will be negligible.' % prog_label)
@@ -752,6 +752,11 @@ class Model(object):
                                                     prog_budget = np.min([default_budget+eps*d_years, prog_budget])
                                                 elif direction < 0:     # Effective budget is decreasing from the default in an absolute manner..
                                                     prog_budget = np.max([default_budget-eps*d_years, prog_budget])
+                                        
+                                        year_check = 2015   # Hard-coded check.
+#                                        if self.sim_settings['tvec'][ti] >= year_check and self.sim_settings['tvec'][ti] < year_check + 0.5*settings.tvec_dt:
+#                                            print prog_label
+#                                            print prog_budget
                                             
                                 else:
                                     if 'saturate_with_default_budgets' in self.sim_settings and self.sim_settings['saturate_with_default_budgets'] is True:
@@ -797,21 +802,24 @@ class Model(object):
                                         else:
                                             impact = prog.getImpact(prog_budget, impact_label = par_label, parser = self.parser, year = self.sim_settings['tvec'][ti], budget_is_coverage = self.sim_settings['alloc_is_coverage'])*source_set_size/source_element_size
                                 
-    #                            year_check = 2020   # Hard-coded check.
-    #                            if self.sim_settings['tvec'][ti] >= year_check and self.sim_settings['tvec'][ti] < year_check + 1.5*settings.tvec_dt:
-    #                                print('Program Name: %s' % prog.name)
-    #                                print('Program %s: %f' % ('Coverage' if self.sim_settings['alloc_is_coverage'] else 'Budget', prog_budget))
-    #                                print('Target Population: %s' % pop.label)
-    #                                print('Target Parameter: %s' % par_label)
-    #                                print('Unit Cost: %f' % prog.func_specs['pars']['unit_cost'])
-    #                                print('Standard Program Impact: %f' % prog.getImpact(prog_budget, budget_is_coverage = self.sim_settings['alloc_is_coverage']))
-    #                                print('Rescaled Program Impact: %f' % prog.getImpact(prog_budget, impact_label = par_label, parser = self.parser, year = self.sim_settings['tvec'][ti], budget_is_coverage = self.sim_settings['alloc_is_coverage']))
-    #                                print('Program Impact Format: %s' % prog.cov_format)
-    #                                print('Source Compartment Size (Target Pop): %f' % source_element_size)
-    #                                print('Source Compartment Size (Aggregated Over Target Pops): %f' % source_set_size)
-    #                                print('Converted Impact: %f' % impact)
-    #                                print('Converted Impact Format: %s' % pars[0].val_format)
-    #                                print
+#                                year_check = 2015   # Hard-coded check.
+#                                if par_label == 'spmyes_rate':
+#                                    if self.sim_settings['tvec'][ti] >= year_check and self.sim_settings['tvec'][ti] < year_check + 0.5*settings.tvec_dt:
+#                                        print('Year: %s' % self.sim_settings['tvec'][ti])
+#                                        print('Program Name: %s' % prog.name)
+#                                        print('Program %s: %f' % ('Coverage' if self.sim_settings['alloc_is_coverage'] else 'Budget', prog_budget))
+#                                        print('Target Population: %s' % pop.label)
+#                                        print('Target Parameter: %s' % par_label)
+#                                        print('Unit Cost: %f' % prog.func_specs['pars']['unit_cost'])
+#                                        print('Standard Program Impact: %f' % prog.getImpact(prog_budget, budget_is_coverage = self.sim_settings['alloc_is_coverage']))
+#                                        print('Rescaled Program Impact: %f' % prog.getImpact(prog_budget, impact_label = par_label, parser = self.parser, year = self.sim_settings['tvec'][ti], budget_is_coverage = self.sim_settings['alloc_is_coverage']))
+#                                        print('Program Impact Format: %s' % prog.cov_format)
+#                                        print('Source Compartment Size (Target Pop): %f' % source_element_size)
+#                                        print('Source Compartment Size (Aggregated Over Target Pops): %f' % source_set_size)
+#                                        print('Converted Impact: %f' % impact)
+#                                        print('Converted Impact Format: %s' % pars[0].val_format)
+#                                        print
+                                        
                                 if first_prog: new_val = 0
                                 new_val += impact
                                 first_prog = False
