c13ae3dbd076ac7bd326b1945038c1a6bc3c1f7c
Azfar <hussainazfar>
2017-06-15 18:33:17 +1000

    setup suite of tests

--- tests/run_alltests.py
+++ tests/run_alltests.py
@@ -1,7 +1,14 @@
-tests = ['project', 'model', 'programs']
+import unittest
+
+testmodules = []
+tests = ['project', 'programs']
 prefix = 'test_'
 suffix = '.py'
 
-for test in tests:
-    test_type  = prefix + test + suffix
-    execfile(test_type)
+for test in tests: testmodules.append(prefix+test)
+
+suite = unittest.TestSuite()
+
+for t in testmodules:
+    suite.addTest(unittest.defaultTestLoader.loadTestsFromName(t))
+test_runner = unittest.TextTestRunner().run(suite)
