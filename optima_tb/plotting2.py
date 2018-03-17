cd7c0cd4387a6ab8ddd6054ec458bb330140add1
Romesh Abeysuriya <romesh.abeysuriya@psych.ox.ac.uk>
2018-03-18 07:35:07 +0800

    Working plotdata implementation

--- optima_tb/plotting2.py
+++ optima_tb/plotting2.py
@@ -50,7 +50,7 @@ class PlotData(object):
     # labels, colours, groupings etc. only apply to plots, not to results, and there could be several
     # different views of the same data.
 
-    def __init__(self,results,outputs,pops,output_aggregation,pop_aggregation,project=None):
+    def __init__(self,results,outputs=None,pops=None,output_aggregation='sum',pop_aggregation='sum',project=None):
         # TODO - Add temporal aggregation
         # Construct a PlotData instance from a Results object by selecting data and optionally
         # specifying desired aggregations
@@ -99,12 +99,12 @@ class PlotData(object):
         if pops is None:
             pops = [pop.label for pop in results[0].model.pops]
         elif pops == 'All':
-            pops = [{'All':[pop.label for pop in results[results.keys()[0]].model.pops]}]
-        elif isinstance(pops,str)
+            pops = [{'All':[pop.label for pop in results[0].model.pops]}]
+        elif isinstance(pops,str):
             pops = [pops]
 
         if outputs is None:
-            outputs = [comp.label for comp in results[results.keys()[0]].model.pops[0].comps if not (comp.tag_birth or comp.tag_dead or comp.is_junction)]
+            outputs = [comp.label for comp in results[0].model.pops[0].comps if not (comp.tag_birth or comp.tag_dead or comp.is_junction)]
         elif isinstance(outputs,str):
             outputs = [outputs]
 
@@ -136,9 +136,9 @@ class PlotData(object):
         self.series = []
         tvecs = dict()
 
-        for result_label,result in results.items(): # For each result
+        for result in results: # For each result
 
-            final_outputs[result_label] = defaultdict(dict) # This final_outputs[result_label][agg_pop_label][agg_output_label] - i.e. the final data for plotting
+            result_label = result.name
             tvecs[result_label] = result.model.sim_settings['tvec']
             dt = result.model.sim_settings['tvec_dt']
 
@@ -216,33 +216,35 @@ class PlotData(object):
                         vals = aggregated_outputs[pop][output_name]
                         self.series.append(Plottable(tvecs[result_label],vals,result_label,pop,output_name))
 
-        self.results = results.keys() # NB. These are lists that thus specify the order in which plotting takes place
+        self.results = [x.name for x in results] # NB. These are lists that thus specify the order in which plotting takes place
         self.pops = [x.keys()[0] if isinstance(x,dict) else x for x in pops]
         self.outputs = [x.keys()[0] if isinstance(x,dict) else x for x in outputs]
 
-    def convert_to_full_names(self,proj):
-        # Change the labels (in place) to full names
-        for s in self.series:
-            s.result = getFullName(s.result,proj)
-            s.pop = getFullName(s.pop,proj)
-            s.output = getFullName(s.output,proj)
-        self.results = [getFullName(r,proj) for r in self.results]
-        self.pops = [getFullName(r,proj) for r in self.pops]
-        self.outputs = [getFullName(r,proj) for r in self.outputs]
+        if project is not None:
+            for s in self.series:
+                s.result = getFullName(s.result,project)
+                s.pop = getFullName(s.pop,project)
+                s.output = getFullName(s.output,project)
+            self.results = [getFullName(r,project) for r in self.results]
+            self.pops = [getFullName(r,project) for r in self.pops]
+            self.outputs = [getFullName(r,project) for r in self.outputs]
+
+    def __repr__(self):
+        s = 'PlotData\n'
+        s += 'Results: %s\n' % (self.results)
+        s += 'Pops: %s\n' % (self.pops)
+        s += 'Outputs: %s\n' % (self.outputs)
+        return s
 
     def __getitem__(self,key):
         # key is a tuple of (result,pop,output)
         # retrive a single Series e.g. plotdata['default','0-4','sus']
         for s in self.series:
-            if s.result == key[0] and s.pop == key[1] and s.output = key[2]:
+            if s.result == key[0] and s.pop == key[1] and s.output == key[2]:
                 return s
         raise OptimaException('Series %s-%s-%s not found' % (key[0],key[1],key[2]))
 
-    def get_xy(self,results=None,pops=None,outputs=None):
-        # Retrieve a matrix of data across keys in one dimension, keeping the other two fixed
-
-
-    def set_colors(self,colors,results=None,pops=None,outputs=None,overwrite=True):
+    def set_colors(self,colors,results=[],pops=[],outputs=[],overwrite=True):
         # Set the colours for plottables matching the filters applied by results, pops, or outputs
         # Colours will be applied across all other dimensions
         # Set the colours in the Plottables manually for further granularity
@@ -252,11 +254,11 @@ class PlotData(object):
 
         assert len(colors) in [len(results),len(pops),len(outputs)]
         
-        for i,color in colors:
+        for i,color in enumerate(colors):
             
-            if results is not None:
+            if results:
                 series = [x for x in self.series if x.result == results[i]] 
-            elif pops is not None:
+            elif pops:
                 series = [x for x in self.series if x.pop == pops[i]] 
             else:
                 series = [x for x in self.series if x.output == outputs[i]] 
@@ -275,7 +277,7 @@ class Plottable(object):
         self.label = label # Automatically decide label at runtime (e.g. in plotSeries this could be the output name or the pop name depending on the axis)
 
 
-def plotSeries(plotdata,plot_type='line',project=None,separate_legend=False,plot_observed_data=True):
+def plotSeries(plotdata,plot_type='line',axis='outputs',separate_legend=False,plot_observed_data=True,project=None):
     # TODO -
     # - Clean up doing aggregation separately
     # - Implement separate figures as everything starting out on the same plot and then
@@ -286,10 +288,9 @@ def plotSeries(plotdata,plot_type='line',project=None,separate_legend=False,plot
     # INPUTS
     # - plotdata - a PlotData instance to plot
     # - plot_type - 'line', 'stacked', or 'proportion' (stacked, normalized to 1)
-    # - project - Attempt to convert population and result labels using a project
-    # - separate_legend - Show the legend in a separate figure,
-    # - plot_observed_data - Draw scatter points for data wherever the output label matches
+    # - project - Draw scatter points for data wherever the output label matches
     #   a data label. Only draws data if the plot_type is 'line'
+    # - separate_legend - Show the legend in a separate figure,
 
     assert axis in ['outputs','results','pops']
 
@@ -297,87 +298,83 @@ def plotSeries(plotdata,plot_type='line',project=None,separate_legend=False,plot
 
     plotdata = dcp(plotdata)
 
-    if project is not None:
-        plotdata.convert_to_full_names(project)
-
     # Update colours with defaults, if they were not set
 
     if axis == 'results':
-        plotdata.set_colors(len(plotdata.pops),pops=plotdata.pops)
+        plotdata.set_colors(gridColorMap(len(plotdata.pops)),pops=plotdata.pops)
     elif axis == 'pops':
-        plotdata.set_colors(len(plotdata.pops),pops=plotdata.pops)
+        plotdata.set_colors(gridColorMap(len(plotdata.pops)),pops=plotdata.pops)
     elif axis == 'outputs':
-        plotdata.set_colors(len(plotdata.outputs),outputs=plotdata.outputs)
+        plotdata.set_colors(gridColorMap(len(plotdata.outputs)),outputs=plotdata.outputs)
 
     if axis == 'results':
         for pop in plotdata.pops:
             for output in plotdata.outputs:
-                figs.append(plt.figure())
-                figs[-1].set_label('%s_%s' % (pop,output))
-                plt.ylabel(name(output,proj))
-                plt.title('%s' % (pop))
+                fig,ax = plt.subplots()
+                fig.set_label('%s_%s' % (pop,output))
+                figs.append(fig)
+                ax.set_ylabel(name(output,proj))
+                ax.set_title('%s' % (pop))
                 if plot_type in ['stacked','proportion']:
                     y = np.stack([plotdata[result,pop,output].vals for result in plotdata.results])
                     y = y/np.sum(y,axis=0) if plot_type == 'proportion' else y
-                    labels = [name(result,proj) for result in plotdata.results]
-                    plt.stackplot(final_tvecs[result],y,labels=labels,colors=colors)
+                    ax.stackplot(plotdata[plotdata.results[0],pop,output].tvec,y,labels=plotdata.results,colors=[plotdata[result,pop,output].color for result in plotdata.results])
                 else:
-                    for result,color in zip(plotdata.results,colors):
-                        plt.plot(final_tvecs[result],plotdata[result,pop,output].vals,color=color,label=name(result,proj))
-                        if plot_observed_data:
-                            plot_data(proj, pop, output,name,color)
-                apply_series_formatting(plot_type)
+                    for result in plotdata.results:
+                        ax.plot(plotdata[result,pop,output].tvec,plotdata[result,pop,output].vals,color=plotdata[result,pop,output].color,label=result)
+                        if project is not None:
+                            render_data(ax,project, pop, output,plotdata[result,pop,output].color)
+                apply_series_formatting(ax,plot_type)
                 if not separate_legend:
-                    render_legend(plot_type)
+                    render_legend(ax,plot_type)
 
     elif axis == 'pops':
         for result in plotdata.results:
             for output in plotdata.outputs:
-                figs.append(plt.figure())
-                figs[-1].set_label('%s_%s' % (result,output))
-                plt.ylabel(name(output,proj))
-                plt.title('%s' % (result))
+                fig,ax = plt.subplots()
+                fig.set_label('%s_%s' % (result,output))
+                figs.append(fig)
+                ax.set_title('%s' % (result))
                 if plot_type in ['stacked','proportion']:
                     y = np.stack([plotdata[result,pop,output].vals for pop in plotdata.pops])
                     y = y/np.sum(y,axis=0) if plot_type == 'proportion' else y
-                    labels = [name(pop,proj) for pop in plotdata.pops]
-                    plt.stackplot(final_tvecs[result],y,labels=labels,colors=colors)
+                    ax.stackplot(plotdata[result,plotdata.pops[0],output].tvec,y,labels=plotdata.pops,colors=[plotdata[result,pop,output].color for pop in plotdata.pops])
                 else:
-                    for pop,color in zip(plotdata.pops,colors):
-                        plt.plot(final_tvecs[result],plotdata[result,pop,output].vals,color=color,label=name(pop,proj))
-                        if plot_observed_data:
-                            plot_data(proj, pop, output,name,color)
-                apply_series_formatting(plot_type)
+                    for pop in plotdata.pops:
+                        ax.plot(plotdata[result,pop,output].tvec,plotdata[result,pop,output].vals,color=plotdata[result,pop,output].color,label=pop)
+                        if project is not None:
+                            render_data(ax,project, pop, output,plotdata[result,pop,output].color)
+                apply_series_formatting(ax,plot_type)
                 if not separate_legend:
-                    render_legend(plot_type,)
+                    render_legend(ax,plot_type,)
 
     elif axis == 'outputs':
         for result in plotdata.results:
             for pop in plotdata.pops:
-                figs.append(plt.figure())
-                figs[-1].set_label('%s_%s' % (result,pop))
+                fig,ax = plt.subplots()
+                fig.set_label('%s_%s' % (result,pop))
+                figs.append(fig)
                 # plt.ylabel('Mixed')
-                plt.title('%s-%s' % (result,name(pop,proj)))
+                ax.set_title('%s-%s' % (result,pop))
                 if plot_type in ['stacked','proportion']:
                     y = np.stack([plotdata[result,pop,output].vals for output in plotdata.outputs])
                     y = y/np.sum(y,axis=0) if plot_type == 'proportion' else y
-                    labels = [name(output,proj) for output in plotdata.outputs]
-                    plt.stackplot(final_tvecs[result],y,labels=labels,colors=colors)
+                    ax.stackplot(plotdata[result,pop,plotdata.outputs[0]].tvec,y,labels=plotdata.outputs,colors=[plotdata[result,pop,output].color for output in plotdata.outputs])
                 else:
-                    for output,color in zip(plotdata.outputs,colors):
-                        plt.plot(final_tvecs[result],plotdata[result,pop,output].vals,color=color,label=name(output,proj))
-                        if plot_observed_data:
-                            plot_data(proj, pop, output,name,color)
-                apply_series_formatting(plot_type)
+                    for output in plotdata.outputs:
+                        ax.plot(plotdata[result,pop,output].tvec,plotdata[result,pop,output].vals,color=plotdata[result,pop,output].color,label=output)
+                        if project is not None:
+                            render_data(ax,project, pop, output,plotdata[result,pop,output].color)
+                apply_series_formatting(ax,plot_type)
                 if not separate_legend:
-                    render_legend(plot_type)
+                    render_legend(ax,plot_type)
 
     if separate_legend:
-        figs.append(render_separate_legend(plot_type))
+        figs.append(render_separate_legend(ax,plot_type))
 
     return figs
 
-def plot_data(proj,pop,output,name,color):
+def render_data(ax,proj,pop,output,color):
     # This function renders a scatter plot for a single variable (in a single population)
     # The scatter plot is drawn in the current axis
     # INPUTS
@@ -401,7 +398,7 @@ def plot_data(proj,pop,output,name,color):
     else:
         return
 
-    plt.scatter(t,y,marker='o', s=40, linewidths=3, facecolors='none',color=color)#label='Data %s %s' % (name(pop,proj),name(output,proj)))
+    ax.scatter(t,y,marker='o', s=40, linewidths=3, facecolors='none',color=color)#label='Data %s %s' % (name(pop,proj),name(output,proj)))
 
 def KMSuffixFormatter(x, pos):
     # This function specifies the formatting for the Y-axis labels
@@ -414,12 +411,11 @@ def KMSuffixFormatter(x, pos):
     else:
         return '%g' % x
 
-def apply_series_formatting(plot_type):
+def apply_series_formatting(ax,plot_type):
     # This function applies formatting that is common to all Series plots
     # (irrespective of the 'axis' setting)
-    ax = plt.gca()
-    plt.autoscale(enable=True, axis='x', tight=True)
-    plt.xlabel('Year')
+    ax.autoscale(enable=True, axis='x', tight=True)
+    ax.set_xlabel('Year')
     ax.set_ylim(ymin=0)
     if plot_type == 'proportion':
         ax.set_ylim(ymax=1)
@@ -428,8 +424,7 @@ def apply_series_formatting(plot_type):
         ax.set_ylim(ymax=ax.get_ylim()[1] * 1.05)
     ax.yaxis.set_major_formatter(FuncFormatter(KMSuffixFormatter))
 
-def render_separate_legend(plot_type):
-    ax = plt.gca()
+def render_separate_legend(ax,plot_type):
     handles, labels_leg = ax.get_legend_handles_labels()
 
     fig = plt.figure()
@@ -454,12 +449,10 @@ def render_separate_legend(plot_type):
 
     return fig
 
-def render_legend(plot_type):
+def render_legend(ax,plot_type):
     # This function renders a legend
     # INPUTS
     # - plot_type - Used to decide whether to reverse the legend order for stackplots
-
-    ax = plt.gca()
     handles, labels_leg = ax.get_legend_handles_labels()
 
     legendsettings = {'loc': 'center left', 'bbox_to_anchor': (1.05, 0.5), 'ncol': 1}
