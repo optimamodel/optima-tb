923833daa099531997b98a836d1ad694f29875f2
Romesh Abeysuriya <romesh.abeysuriya@burnet.edu.au>
2018-05-02 13:55:06 +0800

    Update calibration2 syntax to match Atomica

--- optima_tb/calibration2.py
+++ optima_tb/calibration2.py
@@ -19,34 +19,35 @@ Calibration and sensitivity analysis
 
 """
 
-# CALIBRATION LAYOUT
-# Auto calibration proceeds using y-factors. The input should thus be a list
-# of parameters to calibrate. The output metric would be a list of characteristics
-# or flow rates that need to be matched. So calibration requires
-# - A list of parameters to adjust
-# - A list of output characteristics to match
-#
-# For example, the input parameters could be a list of birth rates and transfer rates
-# and HIV infection rates, and the output parameters could be a list of characteristics
-# In theory these could be population-specific?
-
 def update_parset(parset,y_factors,pars_to_adjust):
+    # Insert updated y-values into the parset
+    # - parset : a ParameterSet object
+    # - y_factors : Array with as many elements as pars_to_adjust
+    # - pars_to_adjust : Array of tuples (par_name,pop_label,...) with special value pop='all' supported
+    #                    Must have as many elements as y_factors. pop=None is not allowed - it must be converted
+    #                    to a full list of pops previously (in performAutofit)
+
     for i,x in enumerate(pars_to_adjust):
-        par_label = x[0]
+        par_name = x[0]
         pop_label = x[1]
 
-        if par_label in parset.par_ids['cascade'] or par_label in parset.par_ids['characs']:
-            parset.update([y_factors[i]], [(par_label,pop_label)], isYFactor=True)
-        else: # For now, must be in there...
-            tokens = par_label.split('_from_')
+        if par_name in parset.par_ids['cascade'] or par_name in parset.par_ids['characs']:
+            par = parset.getPar(par_name)
+            if pop_label == 'all':
+                for pop in par.pops:
+                    par.y_factor[pop] = y_factors[i]
+            else:
+                par.y_factor[pop_label] = y_factors[i]
+        else:
+            tokens = par_name.split('_from_')
             par = parset.transfers[tokens[0]][tokens[1]]
             par.y_factor[pop_label] = y_factors[i]
+
 
 def calculateObjective(y_factors,pars_to_adjust,output_quantities,parset,project):
     # y-factors, array of y-factors to apply to specified output_quantities
     # pars_to_adjust - list of tuples (par_label,pop_label) recognized by parset.update()
     # output_quantities - a tuple like (pop,var,weight,metric) understood by model.getPop[pop].getVar(var)
-    # TODO - support Link flow rates and parameters, need to auto adjust
 
     update_parset(parset,y_factors, pars_to_adjust)
 
@@ -54,12 +55,14 @@ def calculateObjective(y_factors,pars_to_adjust,output_quantities,parset,project
 
     objective = 0.0
 
-    for pop_label,var_label,weight,metric in output_quantities:
+    for var_label,pop_label,weight,metric in output_quantities:
         var = result.model.getPop(pop_label).getVariable(var_label)
-        if isinstance(var[0],Characteristic):
+        if var_label in project.data['characs'].keys():
             target = project.data['characs'][var_label][pop_label]
+        elif var_label in project.data['linkpars'].keys():
+            target = project.data['linkpars'][var_label][pop_label]
         else:
-            raise OptimaException('Not yet implemented')
+            raise NotImplementedError
 
         y = target['y']
         y2 = interpolateFunc(var[0].t,var[0].vals,target['t'])
@@ -67,6 +70,7 @@ def calculateObjective(y_factors,pars_to_adjust,output_quantities,parset,project
         objective += weight* sum(_calculateFitscore(y, y2, metric))
 
     return objective
+
 
 def _getFitscoreFunc(metric):
     """
@@ -94,6 +98,9 @@ def _calc_meansquare(y_obs,y_fit):
     Note: could also use implementation from sklearn in future ... 
     """
     return np.sqrt(((y_fit - y_obs) ** 2).mean())
+
+def _calc_fractional(y_obs,y_fit):
+    return np.abs((y_fit-y_obs)/np.clip(y_obs,1,None)) # Use clipping to handle cases where data has value 0
 
 def _calc_wape(y_obs,y_fit):
     """
@@ -133,7 +140,7 @@ def performAutofit(proj,parset,pars_to_adjust,output_quantities,max_time=60):
     xmin = []
     xmax = []
     for i,x in enumerate(pars_to_adjust):
-        par_label, pop_label, scale = x
+        par_label, pop_label, scale_min, scale_max = x
         if par_label in parset.par_ids['cascade'] or par_label in parset.par_ids['characs']:
             par = parset.getPar(par_label)
             x0.append(par.y_factor[pop_label])
@@ -141,8 +148,8 @@ def performAutofit(proj,parset,pars_to_adjust,output_quantities,max_time=60):
             tokens = par_label.split('_from_')
             par = parset.transfers[tokens[0]][tokens[1]]
             x0.append(par.y_factor[pop_label])
-        xmin.append(1-scale)
-        xmax.append(1+scale)
+        xmin.append(scale_min)
+        xmax.append(scale_max)
 
     optim_args = {
                  'stepsize': proj.settings.autofit_params['stepsize'],
@@ -163,33 +170,101 @@ def performAutofit(proj,parset,pars_to_adjust,output_quantities,max_time=60):
 
     update_parset(args['parset'],x1,pars_to_adjust)
 
+    for i,x in enumerate(pars_to_adjust):
+        par_label = x[0]
+        pop_label = x[1]
+
+        if par_label in parset.par_ids['cascade'] or par_label in parset.par_ids['characs']:
+            par = args['parset'].getPar(par_label)
+
+            if pop_label is None:
+                for pop in par.pops:
+                    print '%s - %s, scale=%.2f' % (par_label, pop, par.y_factor[pop])
+            else:
+                print '%s - %s, scale=%.2f' % (par_label, pop_label, par.y_factor[pop_label])
+
+        else: # For now, must be in there...
+            tokens = par_label.split('_from_')
+            par = args['parset'].transfers[tokens[0]][tokens[1]]
+            print '%s - %s, scale=%.2f' % (tokens[0], tokens[1], par.y_factor[pop_label])
+
+    for i,x in enumerate(pars_to_adjust):
+        par_label = x[0]
+        pop_label = x[1]
+
+        if par_label in parset.par_ids['cascade'] or par_label in parset.par_ids['characs']:
+            par = args['parset'].getPar(par_label)
+
+            if pop_label is None:
+                for pop in par.pops:
+                    # parset.getPar('b_rate').y_factor['0-2']=1.06
+                    # parset.transfers['aging']['0-2'].y_factor['3-14'] = 1.01
+                    #
+                    print "parset.getPar('%s').y_factor['%s']=%.2f"  % (par_label, pop, par.y_factor[pop])
+            else:
+                print "parset.getPar('%s').y_factor['%s']=%.2f" % (par_label, pop_label, par.y_factor[pop_label])
+
+        else: # For now, must be in there...
+            tokens = par_label.split('_from_')
+            par = args['parset'].transfers[tokens[0]][tokens[1]]
+            print "parset.transfers['%s']['%s'].y_factor['%s']=%.2f" % (tokens[0], tokens[1], pop_label,par.y_factor[pop_label])
+
     return args['parset']
 
 
 
 def calibrate_demographics(project,parset,max_time=60):
 
-    # Adjust birth rate
-    birth_pars = [('b_rate',parset.getPar('b_rate').pops[0],0.9)]
+    # Adjust birth rates
+    birth_pars = []
+    for pop in parset.getPar('b_rate').pops:
+        birth_pars.append(('b_rate',pop,0.5,1.9))
+    # birth_pars = [birth_pars[0]] # Don't touch migration actually
 
     # Adjust all transfer parameters
     transfer_pars = []
     for x in parset.transfers.values(): # for transfer type
         for y in x.values(): # for from_pop
             for pop in y.pops:
-                transfer_pars.append((y.label,pop,0.9))
+                transfer_pars.append((y.label,pop,0.1,1.9))
 
-    pars_to_adjust = birth_pars + transfer_pars
+    death_pars = []
+    # for pop in parset.getPar('doth_rate').pops:
+    #     birth_pars.append(('doth_rate',pop,0.6,1.4))
+    death_pars.append(('doth_rate','15-64 (HIV+)',0.5,3.0))
+    death_pars.append(('doth_rate','65+ (HIV+)',0.5,3.0))
+
+
+    pars_to_adjust = birth_pars + transfer_pars + death_pars
 
     # Collate the output demographic quantities (just alive for all pops)
     output_quantities = []
     for pop in parset.pop_labels:
-        output_quantities.append((pop,'alive',1.0,"wape"))
+        output_quantities.append(('alive',pop,1.0,"fractional"))
 
     calibrated_parset = performAutofit(project, parset, pars_to_adjust, output_quantities,max_time=max_time)
 
     return calibrated_parset
 
-# Adjust death rates
+
+def calibrate_foi(project,parset,max_time=60):
+
+    # Adjust force of infection
+    foi_pars = []
+    for pop in parset.getPar('spd_infxness').pops:
+        foi_pars.append(('spd_infxness',pop,0.01,100))
+
+    pars_to_adjust = foi_pars
+
+    # Collate the output demographic quantities (just alive for all pops)
+    output_quantities = []
+    for pop in parset.pop_labels:
+        output_quantities.append((pop,'l_inf',1.0,"fractional"))
+        output_quantities.append((pop,'ac_inf',1.0,"fractional"))
+
+    calibrated_parset = performAutofit(project, parset, pars_to_adjust, output_quantities,max_time=max_time)
+
+    return calibrated_parset
+
 
 
